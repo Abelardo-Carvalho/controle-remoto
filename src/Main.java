a2ff4e031f6f59131f0f67f33050a5fa1e0c17ae
Abelardo-Carvalho <abelardocarvalhoneto@gmail.com>
2024-01-11 12:03:48 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+
+        c.maisVolume();
+        c.play();
+
+        c.abrirMenu();
+        c.fecharMenu();
+
+
+
+    }
+}
